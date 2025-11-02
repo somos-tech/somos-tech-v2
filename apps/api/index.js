79fd89a232ba8d2ddb5a586b30b9e609945d43d5
Fernando Serna <fernandoserna@unknown16677b86f876.home>
2025-11-01 19:01:04 -0700

    remove unused functions from index

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,6 +1,4 @@
 import "./functions/events.js";
-import "./functions/venues.js";
-import "./functions/sponsors.js";
 import "./functions/agent.js";
 import "./functions/groups.js";
 import "./functions/register.js";
