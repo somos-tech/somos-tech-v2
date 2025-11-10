a129f26e503cd6c900bcea0650110e625954f343
Joey Cruz <jcruz@somos.tech>
2025-11-09 20:46:25 -0800

    fix: Move admin function imports to end to resolve loading order

--- apps/api/index.js
+++ apps/api/index.js
@@ -3,10 +3,9 @@ import "./functions/agent.js";
 import "./functions/groups.js";
 import "./functions/register.js";
 import "./functions/GetUserRoles.js";
-import "./functions/adminUsers.js";
-import "./functions/adminUserManagement.js";
 import "./functions/notifications.js";
 import "./functions/users.js";
 import "./functions/health.js";
 import "./functions/healthCheck.js";
-// Deployment trigger: 2025-11-09 20:40:17
+import "./functions/adminUsers.js";
+import "./functions/adminUserManagement.js";
